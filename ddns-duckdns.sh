5dca8e8e0983e2384b257ed24dac18ca9f1274dc
Jari Aalto <jari.aalto@cante.net>
2019-07-08 02:45:48 +0300

    ddns-duckdns.sh: full rewrite

--- ddns-duckdns.sh
+++ ddns-duckdns.sh
@@ -3,20 +3,150 @@
 # crontab -e
 # */30 * * * * ~/.duckdns/duckdns.sh >/dev/null 2>&1
 
+AUTHOR="Jari Aalto <jari.aalto@cante.net>"
+VERSION="2019.0708.0220"
+LICENSE="GPL-2+"
+
 CONF=$HOME/.duckdns
-DOMAIN=$(cat $CONF/domain)
+DOMAINS=$(sed -e 's/[ \t]*//' $CONF/domain) 
 TOKEN=$(cat $CONF/token)
 LOG=$CONF/log
 IP_FILE=$CONF/ip
+VERBOSE_URI="&verbose=true"
 
-ip_old=$(cat $IP_FILE 2> /dev/null)
-ip=$(curl --silent ifconfig.co)  # Get current IP
-url="https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip=$ip"
+HELP="\
+Synopsis: $0 [option]
 
-if [ "$DOMAIN" ] &&
-   [ "$TOKEN" ] &&
-   [ ! "$ip_old" = "$ip" ]
-then
-    echo $ip > $IP_FILE
-    curl --silent --insecure --output $LOG "$url"
-fi
+DESCRIPTION
+  Updates IP address to service duckdns.org
+
+OPTIONS
+  -f, --force    Force update even if IP is same.
+  -v, --verbose  Display verbose output.
+  -V, --version  Display version information and exit.
+  -h, --help     Display short help.
+
+FILES
+  These files must exist:
+
+  $CONF/domain   comma separated list of the subnames
+  $CONF/token    The token of account (see your profile)"
+
+Version ()
+{
+    echo "$VERSION $LICENSE $AUTHOR $HOMEPAGE"
+}
+
+Echo ()
+{
+    [ "$VERBOSE" ] && echo "$*"
+}
+
+Warn ()
+{
+    echo "$*" >&2
+}
+
+Die ()
+{
+    Warn "$*"
+    exit 1
+}
+
+OldIP ()
+{
+    cat $IP_FILE 2> /dev/null
+}
+
+CurrentIP ()
+{
+    curl --silent ifconfig.co
+}
+
+Status ()
+{
+    # Linus commands do not display anything by default but
+    # return status code, like:
+    #
+    # <command> ; echo $?
+
+    if [ "$VERBOSE" ]; then
+	cat $LOG
+    fi
+    
+    grep "^OK" $LOG > /dev/null 2>&1
+}
+
+Main ()
+{
+    while :
+    do
+	case "$1" in
+	    -f | --force)
+		shift
+		force=force
+		;;
+	    -v | --verbose)
+		shift
+		VERBOSE=verbose
+		;;
+	    -h | --help)
+		shift
+		echo "$HELP"
+		return 0
+		;;
+	    -V | --version)
+		shift
+		Version
+		return 0
+		;;
+	    -*) Warn "WARN: Unknown option: $1"
+		shift
+		;;
+	    --) shift
+		break
+		;;
+	    *)  break
+		;;
+	esac
+    done
+
+    if [ ! -d "$CONF" ]; then
+	Die "ERROR: No configuration directory: $CONF"
+    fi
+    
+    if [ ! "$DOMAINS" ]; then
+	Die "ERROR: No domains in: $CONF/domain"
+    fi
+
+    if grep "\." $CONF/domain ; then
+	Die "ERROR: FQDN names not allowed, only subdomains names in: $CONF/domain"
+    fi
+    
+    if [ ! "$TOKEN" ] ; then
+	Die "ERROR: No token id in: $CONF/token"
+
+    fi
+
+    ip_old=$(OldIP)
+    ip=$(CurrentIP)
+    url="https://www.duckdns.org/update?domains=$DOMAINS&token=$TOKEN&ip=$ip$VERBOSE_URI"
+
+    Echo "IP old: $ip_old"
+    Echo "IP now: $ip"
+    
+    if [ ! "$force" ] && [ "$ip_old" = "$ip" ]; then
+	Echo "Info: Nothing to update"
+	return 0
+    else
+	echo $ip > $IP_FILE
+	Echo "Info: Updating..."
+	curl --silent --insecure --output $LOG "$url"
+	Echo "Info: Updating...done"
+	Status
+    fi
+}
+
+Main "$@"
+
+# End of file
